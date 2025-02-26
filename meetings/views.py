0e493878b9550d207aadfe613d6d0d7570bd2349
mroatmeal <jfeind101@gmail.com>
2025-02-26 12:18:32 -0600

    finished meeting planner

--- meetings/views.py
+++ meetings/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Meeting, Room
+from django.forms import modelform_factory
 
 # Create your views here.
 def detail(request, id):
@@ -9,3 +10,15 @@ def detail(request, id):
 
 def rooms_list(request):
     return render(request, "meetings/rooms_list.html", {'rooms': Room.objects.all()})
+
+MeetingForm = modelform_factory(Meeting, exclude=[])
+
+def new(request):
+    if request.method == "POST":
+        form = MeetingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('welcome')
+    else:
+        form = MeetingForm()
+    return render(request, 'meetings/new.html', {"form": form})
